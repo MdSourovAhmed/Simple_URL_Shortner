9a857bf6cf6488782d82c60b980d86ee9ed58f95
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-10-15 23:38:41 +0600

    Update mongoose.js

--- Url-Shortner/backend/mongoose.js
+++ Url-Shortner/backend/mongoose.js
@@ -6,15 +6,13 @@ dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-// const User = "MdSourovAhmedSamin";
-// const Pass = "7111777";
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
   const adminSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   username: { type: String, require: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
@@ -38,14 +36,3 @@ app.listen(port, () => {
 });
 
 
-
-
-
-
-// {
-//   email: 'sourova343@gmail.com',
-//   username: 'MdSourovAhmedSamin',
-//   password: '$2b$10$ZdORrHweJwWrn9.EgGZTxOm4/OKVXBDqL7y/8iu/OeUZHu6n7irGS',
-//   _id: new ObjectId('68c1195979d9cc30006703ce'),
-//   createdAt: 2025-09-10T06:23:21.995Z
-// }
