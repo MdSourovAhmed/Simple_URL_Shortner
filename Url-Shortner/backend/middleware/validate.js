c069b2dd0e19a946000a91c36936d0514ec89cb7
Md. Sourov Ahmed <96908951+MdSourovAhmed@users.noreply.github.com>
2025-10-20 23:51:38 +0600

    Remove commented-out validation middleware code

--- Url-Shortner/backend/middleware/validate.js
+++ Url-Shortner/backend/middleware/validate.js
@@ -30,32 +30,3 @@ module.exports = {
   validateEditUrl,
   checkValidationResult,
 };
-
-// const { body, validationResult } = require('express-validator');
-
-// const validateUrl = [
-//   body('url')
-//     .isURL({ require_tld: true, allow_trailing_dot: false })
-//     .withMessage('Invalid URL'),
-// ];
-
-// const validateAdminLogin = [
-//   body('username').notEmpty().withMessage('Username is required'),
-//   body('password').notEmpty().withMessage('Password is required'),
-// ];
-
-// const validateEditUrl = [
-//   body('longUrl')
-//     .isURL({ require_tld: true, allow_trailing_dot: false })
-//     .withMessage('Invalid URL'),
-// ];
-
-// const checkValidationResult = (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-//   next();
-// };
-
-// module.exports = { validateUrl, validateAdminLogin, validateEditUrl, checkValidationResult };
